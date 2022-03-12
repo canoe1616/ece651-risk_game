5e3285de11738e0e0bfe02f6c79ccaf173d06f6c
Owen Gibson <ogg4@duke.edu>
2022-03-12 11:35:19 -0500

    Adding all rule checker test cases

--- shared/src/test/java/edu/duke/ece651/grp9/risk/shared/AttackRuleCheckerTest.java
+++ shared/src/test/java/edu/duke/ece651/grp9/risk/shared/AttackRuleCheckerTest.java
@@ -34,4 +34,41 @@ public class AttackRuleCheckerTest {
     t1.addNeighbors(t3);
     assertEquals(ruleChecker.checkMyRule(p1, t1, t3, 4), error2);
   }
+
+  @Test
+  public void test_chainedRules() {
+    Player p1 = new Player("red");
+    Player p2 = new Player("green");
+    Territory t1 = new Territory("Two Rivers");
+    t1.setOwner(p1);
+    t1.setUnit(5);
+
+    Territory t2 = new Territory("Tar Valon");
+    t2.setOwner(p2);
+    t2.setUnit(3);
+
+    Territory t3 = new Territory("Falme");
+    t3.setOwner(p1);
+    t3.setUnit(5);
+
+    String error1 = "This action is invalid: Two Rivers is not adjacent to Tar Valon.";
+    String error2 = "This action is invalid: you cannot attack your own Territory.";
+    String error3 = "This action is invalid: you do not own Tar Valon.";
+    String error4 = "This action is invalid: Two Rivers does not have enough units.";
+
+    RuleChecker ruleChecker = new UnitsRuleChecker(new OwnerRuleChecker(new AttackRuleChecker(null)));
+
+    t1.addNeighbors(t3);
+    t3.addNeighbors(t1);
+    assertEquals(ruleChecker.checkAction(p1, t1, t3, 4), error2);
+    assertEquals(ruleChecker.checkAction(p1, t1, t2, 4), error1);
+
+    t1.addNeighbors(t2);
+    t2.addNeighbors(t1);
+    assertEquals(ruleChecker.checkAction(p1, t1, t2, 6), error4);
+    assertEquals(ruleChecker.checkAction(p1, t2, t1, 3), error3);
+
+    assertEquals(ruleChecker.checkAction(p1, t1, t2, 5), null);
+  }
+
 }
---
+++ shared/src/test/java/edu/duke/ece651/grp9/risk/shared/MoveRuleCheckerTest.java
@@ -0,0 +1,58 @@
+package edu.duke.ece651.grp9.risk.shared;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class MoveRuleCheckerTest {
+
+  @Test
+  public void test_checkMyRule() {
+    Player p1 = new Player("red");
+    Player p2 = new Player("green");
+    Territory t1 = new Territory("Two Rivers", 5, p1);
+    t1.setOwner(p1);
+    t1.setUnit(5);
+
+    Territory t2 = new Territory("Tar Valon", 3, p2);
+    t2.setOwner(p2);
+    t2.setUnit(3);
+
+    Territory t3 = new Territory("Falme", 5, p1);
+    t3.setOwner(p1);
+    t3.setUnit(5);
+
+    Territory t4 = new Territory("Tear", 5, p1);
+    t4.setOwner(p1);
+    t4.setUnit(5);
+
+    Territory t5 = new Territory("Andor", 5, p1);
+    t5.setOwner(p1);
+    t5.setUnit(5);
+
+    String error1 = "This action is invalid: Two Rivers is not connected to Tar Valon.";
+    String error2 = "This action is invalid: Two Rivers is not connected to Andor.";
+
+    RuleChecker ruleChecker = new MoveRuleChecker(null);
+    t1.addNeighbors(t2);
+    t2.addNeighbors(t1);
+
+    t1.addNeighbors(t3);
+    t3.addNeighbors(t1);
+
+    t2.addNeighbors(t4);
+    t4.addNeighbors(t2);
+
+    t4.addNeighbors(t5);
+    t5.addNeighbors(t4);
+
+    assertEquals(ruleChecker.checkMyRule(p1, t1, t2, 4), error1);
+    assertEquals(ruleChecker.checkMyRule(p1, t1, t3, 4), null);
+    assertEquals(ruleChecker.checkMyRule(p1, t1, t5, 4), error2);
+
+    t3.addNeighbors(t4);
+    t4.addNeighbors(t3);
+    assertEquals(ruleChecker.checkMyRule(p1, t1, t5, 4), null);
+  }
+}
+
