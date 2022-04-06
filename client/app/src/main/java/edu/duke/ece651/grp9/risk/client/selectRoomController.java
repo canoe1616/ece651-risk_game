bf1acfcfffda852a781c19efacb8470f22a0cfc9
ry82 <ry82@duke.edu>
2022-04-06 01:31:24 -0400

    initialize window between pop up

--- client/app/src/main/java/edu/duke/ece651/grp9/risk/client/selectRoomController.java
+++ client/app/src/main/java/edu/duke/ece651/grp9/risk/client/selectRoomController.java
@@ -17,10 +17,11 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.ResourceBundle;
 
-public class selectRoomController implements Initializable {
+public class selectRoomController {
     private Stage Window;
 
     @FXML Button room1;
@@ -30,43 +31,52 @@ public class selectRoomController implements Initializable {
 
     public  ObjectOutputStream objectOutputStream;
     public  ObjectInputStream objectInputStream;
+    private HashMap<String, Button> ButtonMap;
+
+    private void InitButtonMap(){
+        ButtonMap = new HashMap<>();
+        ButtonMap.put("room1", room1);
+        ButtonMap.put("room2", room2);
+        ButtonMap.put("room3", room3);
+        ButtonMap.put("room4", room4);
+    }
+
+    // initial game room according to the given map
+    public void initialize() {
+        InitButtonMap();
+        try {
+
+            String startOrnot1 = (String) objectInputStream.readObject();
+            String startOrnot2 = (String) objectInputStream.readObject();
+            String startOrnot3 = (String) objectInputStream.readObject();
+            String startOrnot4 = (String) objectInputStream.readObject();
+
+            if (startOrnot1.equals("true")) {
+                System.out.println("ROOM 1 FULL");
+                ButtonMap.get("room1").setDisable(true);
+            }
+            if (startOrnot2.equals("true")) {
+                ButtonMap.get("room2").setDisable(true);
+            }
+            if (startOrnot3.equals("true")) {
+                ButtonMap.get("room3").setDisable(true);
+            }
+            if (startOrnot4.equals("true")) {
+                ButtonMap.get("room4").setDisable(true);
+            }
+        }
+        catch(Exception e){
+            e.getStackTrace();
+        }
+    }
+
 
     public selectRoomController(Stage Window, ObjectInputStream objectInputStream,ObjectOutputStream objectOutputStream ) {
         this.Window = Window;
         System.out.println("input name and password.\n click join");
         this.objectInputStream = objectInputStream;
         this.objectOutputStream = objectOutputStream;
-        try {
-            //3
-            String startOrnot1 = (String) objectInputStream.readObject();
-            String startOrnot2 = (String) objectInputStream.readObject();
-            String startOrnot3 = (String) objectInputStream.readObject();
-            String startOrnot4 = (String) objectInputStream.readObject();
 
-
-            System.out.println(startOrnot1 );
-            System.out.println(startOrnot2);
-            System.out.println(startOrnot3);
-            System.out.println(startOrnot4);
-            startOrnot2 = "true";
-
-            if (startOrnot1.equals("true")) {
-                System.out.println("ROOM 1 FULL");
-                room1.setDisable(true);
-            }
-            if (startOrnot2.equals("true")) {
-                room2.setDisable(true);
-            }
-            if (startOrnot3.equals("true")) {
-                room3.setDisable(true);
-            }
-            if (startOrnot4.equals("true")) {
-                room4.setDisable(true);
-            }
-        }
-        catch(Exception e){
-            e.getStackTrace();
-        }
     }
 
     private void joinRoomHelper(int playerNum, ActionEvent actionEvent) throws IOException {
@@ -75,64 +85,29 @@ public class selectRoomController implements Initializable {
         objectOutputStream.reset();
         objectOutputStream.writeObject(playerNum-1);
 
-        Object source = actionEvent.getSource();
+        FXMLLoader loaderStart = new FXMLLoader(getClass().getResource("/FXML/MapView.fxml"));
+        loaderStart.setControllerFactory(c -> {
+           //  make a map for n players
+            MapFactory mapFactory = new MapFactory();
+            Map map = mapFactory.makeMap(playerNum);
+            HashSet<Player> players = map.getPlayer();
+            Player player = players.iterator().next();
+            MapController mc = new MapController(this.Window, map,player,objectInputStream,objectOutputStream);
+            return mc;
+        });
+        Scene scene = new Scene(loaderStart.load());
+        this.Window.setScene(scene);
+        this.Window.show();
 
-            //read something from the server
-
-//                if (playerNum == 2) {
-//                    //read something from the server
-//
-//                    if(startOrnot.equals(true)){
-//                        btn.setDisable(true);
-//                    }
-//
-//                }
-//                if (playerNum == 3) {
-//                    String startOrnot3 = (String) objectInputStream.readObject();
-//                    if(startOrnot.equals(true)){
-//                        btn.setDisable(true);
-//                    }
-//                }
-//                if(playerNum ==4){
-//                    String startOrnot = (String) objectInputStream.readObject();
-//                    if(startOrnot.equals(true)){
-//                        btn.setDisable(true);
-//                    }
-//                }
-//                if(playerNum ==5){
-//                    String startOrnot = (String) objectInputStream.readObject();
-//                    if(startOrnot.equals(true)){
-//                        btn.setDisable(true);
-//                    }
-//                }
- //           }
-  //          catch(Exception e){
-   //             System.out.println(e);
-           // }
-         //   roomNum.setText(Integer.toString(Integer.parseInt(roomNum.getText()) + 1));
-            FXMLLoader loaderStart = new FXMLLoader(getClass().getResource("/FXML/MapView.fxml"));
-            loaderStart.setControllerFactory(c -> {
-               //  make a map for n players
-                MapFactory mapFactory = new MapFactory();
-                Map map = mapFactory.makeMap(playerNum);
-                HashSet<Player> players = map.getPlayer();
-                Player player = players.iterator().next();
-                MapController mc = new MapController(this.Window, map,player,objectInputStream,objectOutputStream);
-                return mc;
-            });
-            Scene scene = new Scene(loaderStart.load());
-            this.Window.setScene(scene);
-            this.Window.show();
-
-            String unitChecking = unitPopup();
-            try {
-                objectOutputStream.reset();
-                objectOutputStream.writeObject(unitChecking);
-            }
-            catch (IOException e) {
-                System.out.println("cannot send the problem with unitchecking");
-            }
+        String unitChecking = unitPopup();
+        try {
+            objectOutputStream.reset();
+            objectOutputStream.writeObject(unitChecking);
         }
+        catch (IOException e) {
+            System.out.println("cannot send the problem with unitchecking");
+        }
+    }
 
     @FXML
     public void JoinRoom1(ActionEvent actionEvent) throws IOException {
@@ -154,10 +129,7 @@ public class selectRoomController implements Initializable {
         joinRoomHelper(5, actionEvent);
     }
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
 
-    }
 
     public String unitPopup() {
         try {
